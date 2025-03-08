8e72b3033b044b84505102ca104f19083eafa14a
OOHehir <40183643+OOHehir@users.noreply.github.com>
2025-03-08 10:10:43 +0000

    Add constants Check connection

--- upload.py
+++ upload.py
@@ -7,8 +7,13 @@
 import os
 import time
 from samsungtvws import SamsungTVWS
+import sys
 
-IMAGE_DIRECTORY = "/home/owen/Pictures/Dublin '18"
+BASE_DIRECTORY = "/home/projects/samsung_tv/images"
+#IMAGE_DIRECTORY = "home/pictures/Dublin '18"
+#IMAGE_DIRECTORY = BASE_DIRECTORY + "/classic_paintings"
+IMAGE_DIRECTORY = BASE_DIRECTORY + "/space_pics"
+TV_IP_ADDRESS = "192.168.178.28"
 
 # MATTE TYPES (not all matte types work for all images)
 # 'none' 'modernthin' 'modern' 'modernwide' 'flexible' 'shadowbox' 'panoramic' 'triptych' 'mix' 'squares'
@@ -18,10 +23,21 @@ IMAGE_DIRECTORY = "/home/owen/Pictures/Dublin '18"
 # (Jon) I tend to prefer 'warm' or 'apricot'
 
 def upload_image(image_path):
+    ''' Uploads a single image to the TV. '''
     try:
         # Connect to the TV
-        tv = SamsungTVWS(host="192.168.178.41")
+        tv = SamsungTVWS(host=TV_IP_ADDRESS)
         art = tv.art()
+        api_ver = art.get_api_version()
+        if not api_ver:
+            print("Can't contact TV, exiting...")
+            sys.exit(1)
+        else:
+            print(f"Connected to TV: {api_ver}")
+
+        if not os.path.exists(image_path):
+            print(f"File {image_path} does not exist.")
+            sys.exit(1)
 
         # Read the image
         with open(image_path, "rb") as f:
@@ -38,11 +54,14 @@ def upload_image(image_path):
         )
         print(f"Uploaded {image_path}: {response}")
         print(f"{time.ctime()}")
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt: Exiting...")
+        sys.exit(1)
     except Exception as e:
         print(f"Error uploading {image_path}: {e}")
 
 def upload_images_in_directory(directory):
-    # List all .jpg files and sort them alphabetically (so the list on the Frame is in the same order as the list on my machine)
+    ''' Uploads all .jpg images in a directory to the Frame TV. '''
     files = sorted(
         [filename for filename in os.listdir(directory) if filename.lower().endswith(".jpg")]
     )
@@ -51,7 +70,8 @@ def upload_images_in_directory(directory):
     for filename in files:
         full_path = os.path.join(directory, filename)
         upload_image(full_path)
-        # Sleep 2 seconds to let the Frame TV process the image; I found this helps a lot when uploading >25 in sequence
+        # Sleep 2 seconds to let the Frame TV process the image;
+        # I found this helps a lot when uploading >25 in sequence
         time.sleep(2)
 
 # Call the function to upload all images
